bca138337aa13a450d7adefc622d23467d0abd1d
Krish Patel <krishxps@gmail.com>
2024-01-21 17:23:16 -0500

    Input captures

--- test.java
+++ test.java
@@ -3,11 +3,16 @@ import java.util.*;
 
 public class test{
     public static void main(String argc[]){
+
         System.out.print("Your Name: ");
         // Java scanner function to take inputs from user
         Scanner sc = new Scanner(System.in);
-        
-        String s = sc.next();
-        System.out.println("Hello, " + s);
+
+        // Capture input till space
+        // String s = sc.next();
+
+        // Capture input till Next Line
+        String k = sc.nextLine();
+        System.out.println("Hello, " + k);
     }
 }
