6ca270842b9527c232c1cce292c0560c46580be5
Krish Patel <krishxps@gmail.com>
2024-01-21 17:29:31 -0500

    Input functions

--- test.java
+++ test.java
@@ -8,11 +8,21 @@ public class test{
         // Java scanner function to take inputs from user
         Scanner sc = new Scanner(System.in);
 
-        // Capture input till space
+        // // Capture input till space
         // String s = sc.next();
 
-        // Capture input till Next Line
-        String k = sc.nextLine();
+        // // Capture input till Next Line
+        // String k = sc.nextLine();
+
+        // // Capture from user
+        // int k = sc.nextInt();
+        // float k = sc.nextFloat();
+        // byte k = sc.nextByte();
+        // double k = sc.nextDouble();
+        // boolean k = sc.nextBoolean();
+        // short k = sc.nextShort();
+        // long k = sc.nextLong();
+
         System.out.println("Hello, " + k);
     }
 }
