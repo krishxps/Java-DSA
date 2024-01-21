8b7f4acf5f5963a4c1a5c4fcbfb219b9f5d3170e
Krish Patel <krishxps@gmail.com>
2024-01-21 16:54:38 -0500

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -1,5 +1,10 @@
 public class pattern{
     public static void main(String argc[]){
+        // *****
+        // ****
+        // ***
+        // **
+        // *
         for(int i = 0 ; i <  5; i++){
             for(int j = 5; j > (i); j--){
                 System.out.print("*");
