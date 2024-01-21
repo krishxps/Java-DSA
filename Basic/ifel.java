4a95870514a184bfb98d92e30352ad8424568eb4
Krish Patel <krishxps@gmail.com>
2024-01-21 18:56:31 -0500

    Ifel

---
+++ Basic/ifel.java
@@ -0,0 +1,11 @@
+public class ifel {
+    public static void main(String[] args) {
+        int a = 10;
+        if(a >= 10){
+            System.out.println("Inside IF");
+        }
+        else{
+            System.out.println("Inside Else");
+        }
+    }
+}
