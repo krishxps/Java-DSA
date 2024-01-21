10cb0dfecdf48b0377005442891444409ee42306
Krish Patel <krishxps@gmail.com>
2024-01-21 18:34:24 -0500

    Update calc.java

--- Basic/calc.java
+++ Basic/calc.java
@@ -1,3 +1,37 @@
+// import java.util.*;
+
+// public class calc{
+//     public static void main(String argc[]){
+
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Input Two Values: ");
+//         int first = sc.nextInt();
+//         int second = sc.nextInt();
+
+//         System.out.print("Input Operation: ");
+//         String s = sc.next();
+
+//         System.out.print("Result: ");
+//         switch(s){
+//             case "*":
+//                 System.out.println(first * second);
+//                 break;
+//             case "+":
+//                 System.out.println(first + second);
+//                 break;
+//             case "-":
+//                 System.out.println(first - second);
+//                 break;
+//             case "/":
+//                 System.out.println(first / second);
+//                 break;
+//             default:
+//                 System.out.println("ERROR: Input Valid Operation");
+//         }
+        
+//     }
+// } 
+
 import java.util.*;
 
 public class calc{
@@ -6,10 +40,9 @@ public class calc{
         Scanner sc = new Scanner(System.in);
         System.out.print("Input Two Values: ");
         int first = sc.nextInt();
-        int second = sc.nextInt();
-
-        System.out.print("Input Operation: ");
         String s = sc.next();
+        int second = sc.nextInt();
+        
 
         System.out.print("Result: ");
         switch(s){
