04ff1c8a0842def5c387e081ab14990765d76c1e
Krish Patel <krishxps@gmail.com>
2024-01-21 17:33:02 -0500

    First Java Program

---
+++ sumof2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class test{
+    public static void main(String argc[]){
+
+        System.out.print("A and B values: ");
+
+        Scanner sc = new Scanner(System.in);
+
+        int first = sc.nextInt();
+        int second = sc.nextInt();
+
+        System.out.println(first + second);
+    }
+}
