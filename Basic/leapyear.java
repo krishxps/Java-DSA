c22754413844b13656372946e62f5eb48109853a
Krish Patel <krishxps@gmail.com>
2024-01-21 20:01:31 -0500

    Create leapyear.java

---
+++ Basic/leapyear.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class leapyear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("ENTER YEAR TO CHECK THIS IS LEAP YEAR OR NOT: ");
+        int leap = sc.nextInt();
+        boolean isLeapYear = false;
+
+        if (leap % 4 == 0) {
+            if (leap % 100 != 0 || leap % 400 == 0) {
+                isLeapYear = true;
+            }
+        }
+
+        String s = isLeapYear? " ": " NOT ";
+        System.out.println("THIS IS" + s +"LEAP YEAR");
+        sc.close();
+    }
+}
