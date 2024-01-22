74a65ea364c350ee2cb24a15e2124a6e0eb687ae
Krish Patel <krishxps@gmail.com>
2024-01-21 19:05:56 -0500

    hahaha

--- Basic/ifel.java
+++ Basic/ifel.java
@@ -3,11 +3,9 @@ import java.util.Scanner;
 public class ifel {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        do{
-            boolean a = true;
-
-        }
         
+        System.out.print("Input Age: ");
+        int a = scanner.nextInt();
         scanner.close();
 
         if(a <= 10){
