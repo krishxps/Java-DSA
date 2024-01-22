85a824ba10fbfcead9409320b6b6506739223d65
Krish Patel <krishxps@gmail.com>
2024-01-22 14:49:57 -0500

    Update pattern.java

--- Basic/pattern.java
+++ Basic/pattern.java
@@ -47,5 +47,14 @@ public class pattern{
         //     }
         //     System.out.println();
         // }
+
+        // //Half pyramid
+        // int n = 4;
+        // for (int line = 0; line < n; line++) {
+        //     for(int star = 0; star <= line ;star++){
+        //         System.out.print(star + 1);
+        //     }
+        //     System.out.println();
+        // }
     }
 }
