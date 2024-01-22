a90850727a5c85f01b9eedf2593130cf91c941a9
Krish Patel <krishxps@gmail.com>
2024-01-22 16:33:05 -0500

    Update func.java

--- Basic/func.java
+++ Basic/func.java
@@ -3,17 +3,19 @@ import java.util.Scanner;
 public class func {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // float sum = 0;
-        int sum = 0;
-
+        
         // System.out.print("FIRST NUMBER : ");
         // int first = sc.nextInt();
         // System.out.print("SECOND NUMBER : ");
         // int second = sc.nextInt();
-        // float first = 1.1f;
-        // float second = 2.1f;
-        int first = 1;
-        int second = 2;
+        
+        float sum = 0;
+        float first = 1.1f;
+        float second = 2.1f;
+
+        // int sum = 0;
+        // int first = 1;
+        // int second = 2;
         sc.close();
         
         for (int i = 0; i < 10; i++) {
