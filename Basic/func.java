ddec33d456eaa1dc23629b6272c8b64255dcc0e3
Krish Patel <krishxps@gmail.com>
2024-01-22 15:10:22 -0500

    Learning Function

---
+++ Basic/func.java
@@ -0,0 +1,13 @@
+public class func {
+    public static void main(String[] args) {
+        int first = 10;
+        int second = 12;
+        int sum = sum(first,second);
+        System.out.println(sum);
+    }
+
+    public static int sum(int a, int b) {
+        return (a + b);
+    }
+    
+}
