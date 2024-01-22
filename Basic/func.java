ad127134341631f3273882d42edad2f7261edd2e
Krish Patel <krishxps@gmail.com>
2024-01-22 15:13:30 -0500

    Functions

--- Basic/func.java
+++ Basic/func.java
@@ -2,8 +2,15 @@ public class func {
     public static void main(String[] args) {
         int first = 10;
         int second = 12;
-        int sum = sum(first,second);
-        System.out.println(sum);
+        int sum = 0;
+        
+        for (int i = 0; i < 10; i++) {
+            sum = sum(first,second);
+            first++;
+            second++;
+            System.out.println("Int I: "+i+", Sum :"+ sum);
+        }
+        
     }
 
     public static int sum(int a, int b) {
