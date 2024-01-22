f03f82ffe32e35a3e1ef72982b4b2150498f56c5
Krish Patel <krishxps@gmail.com>
2024-01-21 20:20:51 -0500

    Formatted RevNum

--- Basic/revnum.java
+++ Basic/revnum.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 public class revnum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Provide Number to reverse: ");
         int num = sc.nextInt();
-        int temp = 0;
         sc.close();
+
+        int temp = 0;
         System.out.print("Reversed Num: ");
+        
         while (num > 0) {
             temp = num % 10;
             System.out.print(temp);
