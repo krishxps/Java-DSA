6649e0f920a03eb02ef5cefc5c59447395ace849
Krish Patel <krishxps@gmail.com>
2024-01-22 15:46:20 -0500

    Update factorial.java

--- Basic/factorial.java
+++ Basic/factorial.java
@@ -2,32 +2,42 @@ import java.util.Scanner;
 
 public class factorial {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = 0;
 
-        System.out.print("ENTER NUMBER: ");
-        do{
-            num = sc.nextInt();
-            if(num < 0){
-                System.out.print("Please Enter Positive Number: ");
-            }
-        }while(num < 0);
-        sc.close();
+        int num = getInput();
 
         long fac = factorialFind(num);
         System.out.println("Factorial is: " + fac);
+        
     }
 
-    public static long factorialFind(int number){
-        if(number == 0){
+    public static long factorialFind(int number) {
+        if (number == 0) {
             return 1;
         }
         long result = 1;
-        while(number > 0){
+        while (number > 0) {
             result *= number;
             number--;
         }
         return result;
     }
-    
+
+
+    public static int getInput() {
+        Scanner sc = new Scanner(System.in);
+        int num = 0;
+        System.out.print("ENTER NUMBER: ");
+
+        do {
+            num = sc.nextInt();
+            if (num < 0) {
+                System.out.print("Please Enter Positive Number: ");
+            }
+        } while (num < 0);
+
+        sc.close();
+
+        return num;
+    }
+
 }
