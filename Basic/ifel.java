06304651e96cada04852cad8ee6f9051b4030435
Krish Patel <krishxps@gmail.com>
2024-01-21 21:39:31 -0500

    Update ifel.java

--- Basic/ifel.java
+++ Basic/ifel.java
@@ -2,15 +2,14 @@ import java.util.Scanner;
 
 public class ifel {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);        
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Input Age: ");
         int a = scanner.nextInt();
         scanner.close();
 
-        if(a <= 10){
+        if (a <= 10) {
             System.out.println("Age is " + a);
-        }
-        else{
+        } else {
             System.out.println("ERROR: Give valid Kid age");
         }
     }
