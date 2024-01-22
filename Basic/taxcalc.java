3d2a1d112d7402c58c7ebda8447ea007f8ec3b02
Krish Patel <krishxps@gmail.com>
2024-01-21 19:38:14 -0500

    BETTER VERSION

--- Basic/taxcalc.java
+++ Basic/taxcalc.java
@@ -2,18 +2,24 @@ import java.util.Scanner;
 
 public class taxcalc {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("ENTER YOUR INCOME (Annual/Yearly): ");
-        int income = sc.nextInt();
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("ENTER YOUR INCOME (Annual/Yearly): ");
+        int income = scanner.nextInt();
+        scanner.close();
+        
+        int tax = 0;
+        
         if (income < 500000) {
-            System.out.println("Government tax is 0%, so your annual income is " + income);
-        } else if (income < 1000000) {
-            double taxAmount = income * 0.20; 
-            System.out.println("Government tax is 20%, so your annual income is " + (income - taxAmount));
+            ; // Nothing to do
+        } else if (income < 1000000) { 
+            tax = 20;
+            income = (int) (income - (income * 0.20)); 
         } else {
-            double taxAmount = income * 0.30; 
-            System.out.println("Government tax is 30%, so your annual income is " + (income - taxAmount));
+            tax = 30;
+            income = (int) (income - (income * 0.30)); 
         }
+        
+        System.out.println("Government tax is "+ tax +"%, so your annual income after tax is " + income);
     }
 }
