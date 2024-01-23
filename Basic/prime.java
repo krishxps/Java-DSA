5ffb0614f67b204ce1a336b5392395e3e92e98b0
Krish Patel <krishxps@gmail.com>
2024-01-23 13:32:34 -0500

    Get Prime number from range

--- Basic/prime.java
+++ Basic/prime.java
@@ -1,28 +1,38 @@
+import java.util.Scanner;
+
 public class prime {
     public static void main(String[] args) {
-        primeInRange(100);
+        Scanner sc = new Scanner(System.in);
+        int n;
+        try {
+            System.out.print("Enter n: ");
+            n = sc.nextInt();
+            sc.close();
+            primeInRange(n);
+        } catch (Exception e) {
+            System.out.println("Please Enter Valid Input");
+        }
     }
 
     private static boolean isPrime(int n) {
-        if(n == 2){
+        if (n == 2) {
             return true;
         }
         for (int i = 2; i <= Math.sqrt(n); i++) {
-            if(n % i == 0){
+            if (n % i == 0) {
                 return false;
             }
         }
-        return true; 
+        return true;
     }
-    
+
     public static void primeInRange(int n) {
         for (int i = 2; i <= n; i++) {
-            if(isPrime(i)){
+            if (isPrime(i)) {
                 System.out.print(i + " ");
             }
         }
         System.out.println();
     }
-    
-    
+
 }
