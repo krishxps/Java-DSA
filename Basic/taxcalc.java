c91eda703e6cd1bc7085f800119cb59b912e1c13
Krish Patel <krishxps@gmail.com>
2024-01-22 19:42:20 -0500

    Dry

--- Basic/taxcalc.java
+++ Basic/taxcalc.java
@@ -8,21 +8,18 @@ public class taxcalc {
         int income = scanner.nextInt();
         scanner.close();
         
-        int tax = 0;
+        float tax = 0;
         int taxAm = 0;
 
         if (income < 500000) {
             ; // Nothing to do
         } else if (income < 1000000) { 
-            tax = 20;
-            taxAm = (int) (income * 0.20);
-            income = (int) (income - taxAm);
+            tax = 0.20f;    
         } else {
-            tax = 30;
-            taxAm = (int) (income * 0.30);
-            income = (int) (income - taxAm); 
+            tax = 0.30f;
         }
-        
-        System.out.println("Government tax is "+ tax +"% ("+ taxAm +"$), so your annual income after tax is " + income);
+        taxAm = (int) (income * tax);
+        income = (int) (income - taxAm);
+        System.out.println("Government tax is "+ ((int)(tax * 100)) +"% ("+ taxAm +"$), so your annual income after tax is " + income);
     }
 }
