7e8ccff9f79a52c420e421bc215d6d05b374da36
Krish Patel <krishxps@gmail.com>
2024-01-23 13:29:25 -0500

    Get Prime Number in Range
    With help of functions

--- Basic/prime.java
+++ Basic/prime.java
@@ -1,6 +1,6 @@
 public class prime {
     public static void main(String[] args) {
-        System.out.println(isPrime(7));
+        primeInRange(100);
     }
 
     private static boolean isPrime(int n) {
@@ -14,4 +14,15 @@ public class prime {
         }
         return true; 
     }
+    
+    public static void primeInRange(int n) {
+        for (int i = 2; i <= n; i++) {
+            if(isPrime(i)){
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+    
+    
 }
