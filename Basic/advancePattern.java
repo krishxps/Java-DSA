199523034c3aaa2ec460e4fa46e48182e67ef10e
Krish Patel <krishxps@gmail.com>
2024-01-25 18:49:11 -0500

    Update advancePattern.java

--- Basic/advancePattern.java
+++ Basic/advancePattern.java
@@ -1,18 +1,18 @@
 public class advancePattern {
     public static void main(String[] args) {
-        frame(5);
+        frame(5,7);
     }
 
-    public static void frame(int n) {
-        // OUTPUT:
+    public static void frame(int n, int m) {
+        // OUTPUT:(5,5)
         // *****
         // *   *
         // *   *
         // *   *
         // *****
         for (int Line = 1; Line <= n; Line++) {
-            for (int Raw = 1; Raw <= n; Raw++) {
-                if (Line == 1 || Line == n || Raw == 1 || Raw == n) {
+            for (int Raw = 1; Raw <= m; Raw++) {
+                if (Line == 1 || Line == n || Raw == 1 || Raw == m) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
