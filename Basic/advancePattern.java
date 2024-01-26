b7a1f0969319a6ef0e46837abb1ddf8c70ebfddb
Krish Patel <krishxps@gmail.com>
2024-01-25 19:12:43 -0500

    Update advancePattern.java

--- Basic/advancePattern.java
+++ Basic/advancePattern.java
@@ -1,6 +1,27 @@
 public class advancePattern {
     public static void main(String[] args) {
-        frame(5,7);
+        // frame(5,7);
+        inveted(5);
+    }
+
+    public static void inveted(int line){
+        // OUTPUT : (5):   
+        //     *
+        //    **
+        //   ***
+        //  ****
+        // *****
+        line++;
+        for (int i = 1; i < line; i++) {
+            for (int j = 1; j < line - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
     }
 
     public static void frame(int n, int m) {
