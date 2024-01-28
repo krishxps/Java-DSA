cbdd09625f8072803f5da96df48b6c4676dbefc7
Krish Patel <krishxps@gmail.com>
2024-01-28 12:25:30 -0500

    Big test sample

--- Data Structures/reverseArray.java
+++ Data Structures/reverseArray.java
@@ -1,6 +1,6 @@
 public class reverseArray {
     public static void main(String[] args) {
-        int arrays[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int arrays[] = { 0 , 1, 2, 3, 4, 5, 6, 7, 8, 9 , 10 , 11 , 12 ,13 ,14 , 15, 16 ,17 ,18 , 19 , 20};
         reverse(arrays);
         for (int i = 0; i < arrays.length; i++) {
             System.out.print(arrays[i] + " ");
