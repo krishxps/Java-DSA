fdbc8380d8ad29789f3d0c88ca0088ba60b0fdbe
senecastudent <156543479+senecastudent@users.noreply.github.com>
2024-01-27 20:52:19 -0500

    Update reveresArray.java

--- Data Structures/reveresArray.java
+++ Data Structures/reveresArray.java
@@ -1,11 +1,12 @@
-public class reverseArray {
+public class reveresArray {
     public static void main(String[] args) {
-        int arrays[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int arrays[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 , 10 , 11, 12 ,13 ,14 ,15 ,16};
         reverse(arrays);
         for (int i = 0; i < arrays.length; i++) {
             System.out.print(arrays[i] + " ");
         }
     }
+    
     public static void reverse(int numbers[]) {
         int temp = 0;
         int first = 0, last = numbers.length - 1;
