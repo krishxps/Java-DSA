2f614fa6836c6dfed91d2620cfa47f5f13247b23
senecastudent <156543479+senecastudent@users.noreply.github.com>
2024-01-28 16:35:08 -0500

    Update maxSubArraySum.java

--- Data Structures/maxSubArraySum.java
+++ Data Structures/maxSubArraySum.java
@@ -1,11 +1,38 @@
 public class maxSubArraySum {
     public static void main(String[] args) {
-        int numbers[] = { 1 , -2 , 6 , -1 ,3};
-        subArr(numbers);
+        int numbers[] = { 1, -2, 6, -1, 3 };
+        // subArr(numbers);
+        prefixSum(numbers);
+    }
+
+    // Time complexity O(n^2) OPTIMIZED than subArr 
+    public static void prefixSum(int numbers[]) {
+        int maximum = Integer.MIN_VALUE;
+        int n = numbers.length;
+        int prefixSum[] = new int[n];
+        prefixSum[0] = numbers[0];
+
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + numbers[i];
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            int start = i;
+            for (int j = i; j < numbers.length; j++) {
+                int end = j;
+                int currSum = start == 0 ? prefixSum[end] : prefixSum[end] - prefixSum[start - 1];
+                
+                if (maximum < currSum){
+                    maximum = currSum;
+                }
+            }
+        }
+        System.out.println("MAX: "+ maximum);
     }
     
-    // time complexity n3 which is bad
-    public static void subArr(int nums[]){
+
+    // Time complexity O(n^3), which can be optimized further
+    public static void subArr(int nums[]) {
         int maximum = Integer.MIN_VALUE;
         for (int i = 0; i < nums.length; i++) {
             int start = i;
@@ -13,10 +40,10 @@ public class maxSubArraySum {
                 int end = j;
                 int subsum = 0;
                 for (int k = start; k <= end; k++) {
-                    subsum += nums[k]; 
+                    subsum += nums[k];
                 }
                 System.out.println(subsum);
-                if(subsum > maximum){
+                if (subsum > maximum) {
                     maximum = subsum;
                 }
             }
