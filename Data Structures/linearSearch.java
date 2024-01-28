9efacd46a3abd38b31833a99bb36d91d82576144
Krish Patel <krishxps@gmail.com>
2024-01-28 12:26:56 -0500

    Minor UI change

--- Data Structures/linearSearch.java
+++ Data Structures/linearSearch.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class linearSearch {
     public static void main(String[] args) {
         String menu[] = {"dosa","bhaji","pav","bhaji pav","samosa", "chineese noodles","dal bhat", "kadhi", "chana"};
-        System.out.print("ENTER NAME:");
+        System.out.print("ENTER NAME: ");
         Scanner sc = new Scanner(System.in);
         String key = sc.nextLine();
         sc.close();
