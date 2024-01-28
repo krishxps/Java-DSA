0e4f21b4a005c2b96dbf3292571c521dcd0beb15
senecastudent <156543479+senecastudent@users.noreply.github.com>
2024-01-28 14:05:17 -0500

    Update pairs.java

--- Data Structures/pairs.java
+++ Data Structures/pairs.java
@@ -3,13 +3,17 @@ public class pairs {
         int numbers[] = { 2 , 4, 6, 8 ,10};
         printPairs(numbers);
     }
+    
     public static void printPairs(int num[]) {
+        int totalPair = 0;
         for (int i = 0; i < num.length; i++) {
             int current = num[i];
             for (int j = 1+i; j < num.length; j++) {
                 System.out.print("(" + current + "," + num[j] + ") ");
+                totalPair++;
             }
             System.out.println();
         }
+        System.out.println("TOTAL PAIRS: " + totalPair + "\n");
     }
 }
