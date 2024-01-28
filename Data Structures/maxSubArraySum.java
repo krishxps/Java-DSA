7b102e68994878d7d3ddcb29f5952307625bdf8c
senecastudent <156543479+senecastudent@users.noreply.github.com>
2024-01-28 16:57:46 -0500

    Kidane's Algorithm

--- Data Structures/maxSubArraySum.java
+++ Data Structures/maxSubArraySum.java
@@ -1,8 +1,41 @@
 public class maxSubArraySum {
     public static void main(String[] args) {
         int numbers[] = { 1, -2, 6, -1, 3 };
+        int nums[] = { -2 , -3 , 4, -1, -2 , 1 , 5 , -3};
+        int nags[] = { -2 , -3 , -4, -1};
         // subArr(numbers);
-        prefixSum(numbers);
+        // prefixSum(numbers);
+        kadanesAlgo(nags);
+    }
+    // tc : O(n) ,  space complexity is O(1),
+    public static void kadanesAlgo(int nums[]) {
+        int cs = 0;
+        int ms = Integer.MIN_VALUE;
+
+        //For Nagative Numbers calculations
+        int biggestNagative = ms;
+        int allnagative = 0;
+        // Check if all nums are nagative
+        for (int i = 0; i < nums.length && allnagative != 1; i++) {
+            if(nums[i] > 0){
+                allnagative = 1;
+            }else if(biggestNagative < nums[i]){
+                biggestNagative = nums[i];
+            }
+        }
+        
+        if(allnagative == 1){
+            for (int i = 0; i < nums.length; i++) {
+                cs += nums[i];
+                if(cs < 0){
+                    cs = 0;
+                }
+                ms = Math.max(cs,ms);
+            }
+            System.out.println("out max subarrays sum is: "+ms);
+        }else{
+            System.out.println("out max subarrays sum is all nagative : "+ biggestNagative);
+        }
     }
 
     // Time complexity O(n^2) OPTIMIZED than subArr 
