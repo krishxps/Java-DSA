59cf05f529525f518876c7329b7a5b6126bed225
Krish Patel <krishxps@gmail.com>
2024-02-13 18:55:46 -0500

    Bubble Sort O^n2

--- Algorithms/01-bubbleSort.java
+++ Algorithms/01-bubbleSort.java
@@ -3,27 +3,41 @@ package Algorithms;
 public class bubbleSort {
     public static void main(String[] args) {
         int arr[] = { 5, 4, 1, 3, 2 };
-        bubble(arr, arr.length);
+        bubble(arr);
+        print(arr);
+        
+        int arr2[] = { 1, 2, 3, 4, 5 };
+        bubble(arr2);
+        print(arr2);
+    }
+
+    // O^n2
+    private static void bubble(int[] arr) {
+        int size = arr.length;
+
+        for (int i = 0; i < size - 1; i++) {
+            int swap = 0;
+
+            for (int j = 0; j < size - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swap++;
+                }
+            }
+
+            if (swap == 0) {
+                break; // Break the loop if no swaps were made, indicating the array is already sorted
+            }
+        }
+    }
+
+    public static void print(int[] arr) {
         for (int i : arr) {
             System.out.print(i + " ");
         }
         System.out.println();
-    }
-
-    // O^n2 
-    private static void bubble(int[] arr, int length) 
-    {
-        int size = length;
-        for (int idx = 0; idx < length; idx++) 
-        {
-            for (int index = 1; index < size - idx; index++) 
-            {
-                if (arr[index - 1] > arr[index]) {
-                    int temp = arr[index - 1];
-                    arr[index - 1] = arr[index];
-                    arr[index] = temp;
-                }
-            }
-        }
     }
 }
